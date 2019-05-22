c5de0d2eeaaacfff3dd672bbf7866f50cc8964cb
Sebastian Roth <sebastian.roth@gmail.com>
2019-05-22 13:08:04 +0100

    Ensure result is delivered back on the main thread

--- android/src/main/java/asia/ivity/h264/H264Plugin.java
+++ android/src/main/java/asia/ivity/h264/H264Plugin.java
@@ -2,6 +2,8 @@ package asia.ivity.h264;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import asia.ivity.h264.util.H264Reader;
 import io.flutter.plugin.common.MethodCall;
@@ -12,7 +14,7 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.util.Map;
+import java.io.IOException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -23,6 +25,8 @@ public class H264Plugin implements MethodCallHandler {
 
   private static final String TAG = "H264Plugin";
   private final Executor backgroundExecutor = Executors.newSingleThreadExecutor();
+
+  private Handler mainThreadHandler;
 
   /**
    * Plugin registration.
@@ -35,36 +39,45 @@ public class H264Plugin implements MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("decode")) {
-      final Map<String, Object> params = (Map<String, Object>) call.arguments;
+    if (mainThreadHandler == null) {
+      mainThreadHandler = new Handler(Looper.getMainLooper());
+    }
 
-      backgroundExecutor.execute(() -> handleDecode(params, result));
+    if (call.method.equals("decode")) {
+      backgroundExecutor.execute(() -> handleDecode(
+          call.argument("source"),
+          call.argument("width"),
+          call.argument("height"),
+          call.argument("target"),
+          result,
+          mainThreadHandler
+      ));
     } else {
       result.notImplemented();
     }
   }
 
-  private void handleDecode(Map<String, Object> params, Result result) {
+  private void handleDecode(String sourceFile, Integer width, Integer height, String targetFile,
+      Result result, Handler resultHandler) {
     Log.d(TAG, "Starting a decoding process");
     try {
-      final File source = new File((String) params.get("source"));
+      final File source = new File(sourceFile);
       final FileInputStream fis = new FileInputStream(source);
 
-      Bitmap decode = H264Reader.decode(fis, source.length(), (Integer) params.get("width"),
-          (Integer) params.get("height"));
+      Bitmap decode = H264Reader.decode(fis, source.length(), width, height);
 
       if (decode != null) {
-        final File target = new File((String) params.get("target"));
+        final File target = new File(targetFile);
         FileOutputStream fos = new FileOutputStream(target);
         decode.compress(CompressFormat.JPEG, 80, fos);
         fos.close();
 
-        result.success(target.getAbsolutePath());
+        resultHandler.post(() -> result.success(target.getAbsolutePath()));
       } else {
-        result.error("h264", "", null);
+        throw new IOException("Can not decode the result");
       }
     } catch (Exception e) {
-      result.error("h264", e.getMessage(), null);
+      resultHandler.post(() -> result.error("h264", e.getMessage(), null));
     }
   }
 }
